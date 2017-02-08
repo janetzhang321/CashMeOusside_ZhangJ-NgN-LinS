ec3df12efb01bebe33fde03671c76f4b794742d6
Sharon <sharonlin14@gmail.com>
2017-02-07 22:55:36 -0500

    Added new functions

--- pt2.py
+++ pt2.py
@@ -1,51 +1,16 @@
-
 from pymongo import MongoClient
+from csv import reader, DictReader
 
 server = MongoClient('homer.stuy.edu')
 db = server.CashMeOusside
 
+def average(L):
+    if len(L) != 0: 
+        return sum(L) / len(L)
+    else:
+        return 0
 
-d = db.teachers
-c= db.students
+peeps = [peep for peep in db.student.find()]
 
-vals = {}
-counter = 0
-
-f = open('teachers.csv').read().split('\n')[1:]
-
-for line in f:
-    if line != "":
-        m = c.find({'courses': {'name': line.split(',')[0] }})
-        for person in m:
-            print("--------------------")        
-            print person
-            print("--------------------")
-        doc = {'name': line.split(',')[1], 'class': line.split(',')[0], 'period':line.split(',')[2], 'students':""}
-        vals[counter] = doc
-        counter += 1
-
-    
-#------------------------------------------------------------------
-
-
-l = c.find()
-
-for person in l:#prints the names
-    try:
-        print person['name']
-        grades = person['courses'].values()
-        a=[]
-        for item in grades:
-            a.append(int(item))
-        l=0
-        for score in a:
-            l += score
-        pavg = float(l)/len(a)
-        print pavg
-        print person['id']
-        print person
-        print '\n'
-    except:
-        {}
-
-
+for peep in peeps:
+    print peep['name'], peep['id'], average([int(course['mark']) for course in peep['grades']])
