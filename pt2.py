8b38e09fe4095aed82d4d0838a707c6fa5a3aabc
Sharon Lin <sharonlin14@gmail.com>
2017-02-07 23:11:34 -0500

    fixed pt2

--- pt2.py
+++ pt2.py
@@ -10,7 +10,10 @@ def average(L):
     else:
         return 0
 
+print("Name, ID, Average")    
+    
 peeps = [peep for peep in db.students.find()]
+peeps = peeps[0:len(peeps)/2]
 
 for peep in peeps:
     print peep['name'], peep['id'], average([int(course['mark']) for course in peep['grades']])
